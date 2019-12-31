d7d95306da20d2c6e382cb2db3c08370d227c372
Bec Scott <me@becdetat.com>
2019-12-31 21:58:26 +1000

    Create a REPL

---
+++ javascript/repl.js
@@ -0,0 +1,28 @@
+const readline = require( 'readline' );
+const performStep = require('./performStep');
+
+const rli = readline.createInterface( process.stdin, process.stdout );
+
+const prompt = ( lastPosition ) => {
+  return new Promise( ( resolve, reject ) => {
+    rli.question(
+      '> ',
+      ( instruction ) => {
+        const newPosition = performStep( lastPosition, instruction );
+
+        return resolve( newPosition );
+      }
+    );
+  } );
+};
+
+const main = async () => {
+  let position = { x: 0, y: 0, f: 'NORTH' };
+
+  while ( true ) {
+    position = await prompt( position );
+    console.log(`🤖 ${JSON.stringify( position )}`)
+  }
+};
+
+main();
