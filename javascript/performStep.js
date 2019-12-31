b96b867bb78fb07fffcac59b5d79593b6dbb63b2
Bec Scott <me@becdetat.com>
2019-12-31 15:35:43 +1000

    Implement PLACE instruction

--- javascript/performStep.js
+++ javascript/performStep.js
@@ -1,8 +1,54 @@
+const BOARD_NS_SIZE = 5;
+const BOARD_WE_SIZE = 5;
+
+function copy(position) {
+  return { ...position };
+}
+
+function isValidFacingDirection( f ) {
+  return 'NESW'.indexOf( f ) !== -1;
+}
+
+function isXOnBoard( x ) {
+  return x >= 0 && x < BOARD_NS_SIZE;
+}
+
+function isYOnBoard( y ) {
+  return y >= 0 && y < BOARD_WE_SIZE;
+}
+
 module.exports = function performStep(initialPosition, instruction) {
   switch ( instruction ) {
     case 'NOOP':
-      return { ...initialPosition };
+      return copy( initialPosition );
   }
 
-  return { ...initialPosition };
+  if ( instruction.startsWith( 'PLACE' ) ) {
+    const slicedInstruction = instruction.slice( 6 );
+
+    if ( slicedInstruction.length != 5 ) {
+      return copy( initialPosition );
+    }
+
+    const [ x, y, f ] = slicedInstruction.split( ',' );
+    const parsedX = parseInt( x );
+    const parsedY = parseInt( y );
+
+    if ( isNaN( parsedX )
+         || isNaN( parsedY )
+         || ! isValidFacingDirection( f )
+         || ! isXOnBoard( parsedX )
+         || ! isYOnBoard( parsedY )
+       ) {
+      return copy( initialPosition );
+    }
+
+    return {
+      x: parsedX,
+      y: parsedY,
+      f
+    };
+  }
+
+  return copy( initialPosition );
 }
--- javascript/performStep.test.js
+++ javascript/performStep.test.js
@@ -1,11 +1,93 @@
 const performStep = require('./performStep');
 
-test( 'performStep with a NOOP', () => {
+test( 'NOOP', () => {
   const initialPosition = { x: 1, y: 2, f: 'N' };
 
-  const newPosition = performStep(initialPosition, 'NOOP');
+  const newPosition = performStep( initialPosition, 'NOOP' );
 
-  expect( newPosition.x ).toBe( 1 );
-  expect( newPosition.y ).toBe( 2 );
-  expect( newPosition.f ).toBe( 'N' );
+  expect( newPosition ).toMatchObject( { x: 1, y: 2, f: 'N' } );
+} );
+
+function getOriginPosition() {
+  return { x: 0, y: 0, f: 'N' };
+}
+
+test( 'PLACE within table', () => {
+  const position = performStep(
+    getOriginPosition(),
+    'PLACE 3,4,S'
+  );
+
+  expect( position ).toMatchObject( { x: 3, y: 4, f: 'S' } );
+} );
+
+test( 'PLACE with missing value is ignored', () => {
+  const position = performStep(
+    getOriginPosition(),
+    'PLACE 3,4'
+  );
+
+  expect( position ).toMatchObject( getOriginPosition() );
+} );
+
+test( 'PLACE with malformed X coordinate is ignored', () => {
+  const position = performStep(
+    getOriginPosition(),
+    'PLACE N,3,S'
+  );
+
+  expect( position ).toMatchObject( getOriginPosition() );
+} );
+test( 'PLACE with malformed Y coordinate is ignored', () => {
+  const position = performStep(
+    getOriginPosition(),
+    'PLACE 3,N,S'
+  );
+
+  expect( position ).toMatchObject( getOriginPosition() );
+} );
+
+test( 'PLACE with malformed facing direction is ignored', () => {
+  const position = performStep(
+    getOriginPosition(),
+    'PLACE 3,3,X'
+  );
+
+  expect( position ).toMatchObject( getOriginPosition() );
+} );
+
+test( 'PLACE cannot place outside X boundary in negative direction', () => {
+  const position = performStep(
+    getOriginPosition(),
+    'PLACE -1,3,N'
+  );
+
+  expect( position ).toMatchObject( getOriginPosition() );
+} );
+
+test( 'PLACE cannot place outside X boundary in positive direction', () => {
+  const position = performStep(
+    getOriginPosition(),
+    'PLACE 6,3,N'
+  );
+
+  expect( position ).toMatchObject( getOriginPosition() );
+} );
+
+test( 'PLACE cannot place outside Y boundary in negative direction', () => {
+  const position = performStep(
+    getOriginPosition(),
+    'PLACE 1,-3,N'
+  );
+
+  expect( position ).toMatchObject( getOriginPosition() );
+} );
+
+test( 'PLACE cannot place outside Y boundary in positive direction', () => {
+  const position = performStep(
+    getOriginPosition(),
+    'PLACE 3,6,N'
+  );
+
+  expect( position ).toMatchObject( getOriginPosition() );
 } );
